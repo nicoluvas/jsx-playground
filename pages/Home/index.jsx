57e4c9ee37bea8da68ebb2c6537aeabb7ff4957a
nicolas <79096051+nicoluvas@users.noreply.github.com>
2022-02-24 22:25:49 -0300

    home page update yeet
    added 'return-to-login' button and some other stylish features

--- pages/Home/index.jsx
+++ pages/Home/index.jsx
@@ -7,14 +7,16 @@ import { Link } from "react-router-dom";
 
 export const Home = () => {
   
+    //
 
     return (
-
-    <div className="home-main">
         
+    <div className="home-main">
+        <Link className="home-to-login-btn" to="/login">
+            <button className="home-to-login-btn">Return to login</button>
+        </Link>
         <footer className="home-footer">Test</footer>
     </div>
-
     );
   };
   
