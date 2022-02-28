e7049ac20685bf65479772f88036aa2c6219ba60
nicolas <79096051+nicoluvas@users.noreply.github.com>
2022-02-28 15:29:32 -0300

    dumb bug fix

--- index.jsx
+++ index.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 
-import { App } from './App';
+import '../../assets/App.css'
+import './home.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
+export const Home = () => {
+  
+    //
 
+    return (
+        
+    <div className="home-main">
+
+        <div className="home-navbar">
+            <Link className="btn-to-login" to="/login">
+                <button className="btn-to-login" >LOG OUT</button>
+            </Link>
+        </div>
+        
+        <footer className="home-footer">Test</footer>
+    </div>
+    );
+  };
+  
