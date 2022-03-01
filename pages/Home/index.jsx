777b5b88179b21c5f1bce06e8a4b526bf4e70abb
nicolas <79096051+nicoluvas@users.noreply.github.com>
2022-03-01 12:14:19 -0300

    new header
    yeet

--- pages/Home/index.jsx
+++ pages/Home/index.jsx
@@ -12,12 +12,19 @@ export const Home = () => {
     return (
         
     <div className="home-main">
-
-        <div className="home-navbar">
+    <header className="home-header">
+        <nav className="home-navbar">
             <Link className="btn-to-login" to="/login">
                 <button className="btn-to-login" >LOG OUT</button>
             </Link>
-        </div>
+            <Link className="btn-to-login" to="/login">
+                <button className="btn-to-login" >LOG OUT</button>
+            </Link>
+            <Link className="btn-to-login" to="/login">
+                <button className="btn-to-login" >LOG OUT</button>
+            </Link>
+        </nav>
+    </header>
         
         <footer className="home-footer">Test</footer>
     </div>
