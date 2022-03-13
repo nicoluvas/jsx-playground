1149d21d833ecb4146286321aab9614634e76fd8
nicolas <79096051+nicoluvas@users.noreply.github.com>
2022-03-13 18:58:09 -0300

    im back 🎷🦙
    finally finding a way to do a decent navbar

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,21 +1,24 @@
-import "../../assets/App.css";
 import "./home.css";
+
+import Banner from '../../assets/Banner.png';
 
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { MenuItems } from "../../components/Navbar/MenuItems.js";
 
 export const Home = () => {
-  //
-
   return (
     <div className="home-main">
-        <nav className="home-navbar">
-            <ul>
-                <li><a className={item.cName}></a></li>
-            </ul>
+      <header>
+        <img className="logo" src={Banner}></img>
+        <nav>
+          <ul className="home-navbar">
+            <li><a href="#">Info</a></li>
+            <li><a href="#">Github</a></li>
+          </ul>
         </nav>
-      <footer className="home-footer">Test</footer>
+        <a className="repo-btn" href="#"><button>Log out</button></a>
+      </header>
     </div>
   );
 };
